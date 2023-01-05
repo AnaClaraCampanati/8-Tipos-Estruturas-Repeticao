269ef279f7f641f6f773fbbdc3e56f363ffc044a
Ana Clara campanati <campanatianaclara@gmail.com>
2023-01-05 13:39:14 -0300

    Adicionar arquivos de projeto.

---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿// Faça um programa em C# que imprima a soma dos números de 1 até 1000.
+
+int soma = 0;
+
+for (int i = 1; i <= 1000; i++)
+{
+    soma = soma + i;
+}
+
+Console.WriteLine("A soma dos números é: " + soma);
